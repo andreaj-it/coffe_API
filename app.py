fd37e660ac887a0f8f90a0ea85e6d70ae850fe87
andrea <andrea@trillonario.com>
2022-08-14 22:00:01 -0300

    Correccion Ejercicio

--- app.py
+++ app.py
@@ -3,29 +3,53 @@ import pickle
 import pandas as pd
 import numpy as np
 
-app = Flask(__name__)
-model = pickle.load(open('coffee_model.pkl', 'rb'))
+app = Flask(__name__,template_folder='templates')
 
 @app.route('/')
 
 def home():
     return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
+@app.route('/', methods=['POST'])
 
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+    if request.method == 'POST':
+        country = request.form['country']
+        variety = request.form['variety']
+        aroma = request.form['aroma']
+        aftertaste = request.form['aftertaste']
+        acidity = request.form['acidity']
+        body = request.form['body']
+        balance = request.form['balance']
+        moisture = request.form['moisture']
 
-    output = round(prediction[0], 2)
+        cols = ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture']
+        data = [country,variety,aroma,aftertaste,acidity,body,balance,moisture]
+        to_predict_list = pd.DataFrame(np.array(data).reshape(1,8), columns=cols) #fila con 8 columnas, lo q va a recibir
 
-    return render_template('index.html', prediction_text='Como es el cafe? {} (Yes=Es un cafe de primera, No=No es cafe de primera)'.format(output))
+        #to_predict_list = request.form.to_dict()
+        #to_predict_list=list(to_predict_list.values())
+        #to_predict_list = list(map(int, to_predict_list))
+        
+        result = ValuePredictor(to_predict_list)
+        
+        if result == 'Yes':
+            prediction='Es un cafe de primera'
+        else:
+            prediction='NO es un cafe de primera'
+            
+        return render_template("index.html",prediction=prediction)
+
+def ValuePredictor(to_predict_list):
+    to_predict = to_predict_list
+    #to_predict = np.array(to_predict_list).reshape(1,8)
+    loaded_model = pickle.load(open("coffee_model.pkl","rb"))
+    #model = pickle.load(open('coffee_model.pkl', 'rb'))
+    result = loaded_model.predict(to_predict)
+    return result[0]
 
 """
+example = ['Other', 'Other', 7.42, 7.33, 7.42, 7.25, 7.33, 0]
 @app.route('/<string:country>/<string:variety>/<float:aroma>/<float:aftertaste>/<float:acidity>/<float:body>/<float:balance>/<float:moisture>/')
 def result(country,variety,aroma,aftertaste,acidity,body,balance,moisture):
     cols = ['country_of_origin','variety','aroma','aftertaste','acidity','body','balance','moisture']
